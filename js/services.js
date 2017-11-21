8d266de480673ad12b16a7244111a3ebf2f7950b
elia9090 <elianarducciweb@gmail.com>
2017-11-21 15:09:01 +0100

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,70 +1,7 @@
 
 //Causa problemi se inserito in util
 
-//Services
-function getTktDataByFilter( offset='' , limit='', filter=null, sort=null ){
-//    console.log('getTktDataByFilter -> Inizio elaborazione');
-    var err;
-    var filters = {};
-    //alert('offset: '+offset+' limit: '+limit);
-    if( offset != '' ){
-        filters.offset = offset;
-    }
-    if( limit != '' ){
-        filters.limit = limit;
-    }
-    if( filter != null ){
-        var fooFilter = {};
-        Object.keys(filter).forEach(function(key) {
-            //console.log(key, filter[key]);
-            fooFilter[key] = filter[key];
-        });
-        filters.filter = fooFilter;
-        //console.log(filters.filter);
-    }
-    if( sort != null ){
-        var fooSort = {};
-        Object.keys(sort).forEach(function(key) {
-            //console.log(key, filter[key]);
-            fooSort[key] = sort[key];
-        });
-        filters.sort = fooSort;
-        //console.log(filters.filter);
-    }
-    $$.ajax({
-        headers: {
-            'Authorization': 'Bearer 102-token',
-            'Access-Control-Allow-Origin': '*',
-            'Content-type': 'application/x-www-form-urlencoded',
-//            'dataType':'json',
-        },
-        data: filters,
-        async: false, //needed if you want to populate variable directly without an additional callback
-        url: 'http://192.168.3.9/v2/ttm/listfilters',
-        method: 'POST',
-        dataType: 'json', //compulsory to receive values as an object
-        processData: true, //ignore parameters if sets to false
-        //contentType: 'application/x-www-form-urlencoded',
-        crossDomain: true,
-            error: function (data, status, xhr) {
 
-                //alert(JSON.stringify(data));
-                myApp.alert('Nessun dato da caricare');
-                err = 'err_00'
-            },
-            success: function (data, status, xhr) {
-
-                myList = data;
-            },
-        statusCode: {
-
-            401: function (xhr) {
-                alert('App non autorizzata ad ottenere i dati');
-            }
-        }
-    });
-    return myList;
-}
 
 function getMaximoTktList(stringFilter){
     var err;
@@ -185,9 +122,13 @@ function getUserInfo(){
             
         }
 }
-function validateUser(uuid='',upwd=''){
+function validateUser(uuid,upwd){
     var chkLogin = false;
-
+    if(!uuid || !upwd){
+        myApp.alert("Inserire username e password","Login errato");
+        userAndPwdCheck = false;
+        return;
+    }
     $$.ajax({
         headers: {
 //            'Authorization': 'Bearer 102-token',
