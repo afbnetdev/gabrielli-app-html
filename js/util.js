aea79e159129fa9c3e37da1887f9e09d5e3ee3a7
elia9090 <elianarducciweb@gmail.com>
2017-11-28 14:29:40 +0100

    Update util.js

--- js/util.js
+++ js/util.js
@@ -4,7 +4,7 @@ Initial setup
  ---------------------------------------*/
 
 var URL_ENDPOINT = 'http://portal.gabriellispa.it';
-//var URL_ENDPOINT = 'http://192.168.2.90:10039';
+//var URL_ENDPOINT = 'http://192.168.2.90:9080';
 
 //FILTER STRING
 var pageSizeFilterTickets=20;
@@ -117,25 +117,26 @@ function buildDocumentTable(myList, columns, limit, lastIndexDoc) {
 
     for (var i = 0; i < filterMyList.length; i++) {
         var row$ = $$('<tr/>');
-        //DA MODIFICARE L'EMAIL DELLA RISPOSTA CON I VALORI EFFETTIVAMENTE RESTITUITI DAL JSON
+        //DA MODIFICARE L'EMAIL DELLA RISPOSTA CON I VALORI EFFETTIVAMENTE RESTITUITI DAL JSON 
         // PER REVERT SOSTITUIRE EX filterMyList[i].NumeroDocumento CON filterMyList[i].docNumber ECC...
-        var cellValue = {'docNumber': filterMyList[i].NumeroDocumento, 'docTitle': filterMyList[i].Title, 'docDate': filterMyList[i].DataDocumento, 'docLinkEmail': filterMyList[i].doclink, 'docPdf_Key_Doc_RISFA': filterMyList[i].KEY_DOC,'docPdf_LinkUrl_SHARE_POINT': filterMyList[i].LinkUrl}
+        var cellValue = {'docNumber': filterMyList[i].NumeroDocumento, 'docTitle': filterMyList[i].Title, 'docDate': filterMyList[i].DataDocumento, 'docImporto': filterMyList[i].Importo, 'docLinkEmail': filterMyList[i].doclink, 'docPdf_Key_Doc_RISFA': filterMyList[i].KEY_DOC,'docPdf_LinkUrl_SHARE_POINT': filterMyList[i].LinkUrl}
         row$.append($$('<td data-collapsible-title="' + columns[0] + '"/>').html('<a href="#" class="doc-info_number">' + cellValue.docNumber + '</a>'));
         row$.append($$('<td data-collapsible-title="' + columns[1] + '"/>').html('<a href="#" class="doc-info_title">' + cellValue.docTitle + '</a>'));
         row$.append($$('<td data-collapsible-title="' + columns[2] + '"/>').html('<a href="#" class="doc-info_date">' + formatDateFromTimeStampToItalian(cellValue.docDate) + '</a>'));
+        row$.append($$('<td data-collapsible-title="' + columns[3] + '"/>').html('<a href="#" class="doc-info_importo"> &#8364; ' + formatAmountToFloat(cellValue.docImporto) + '</a>'));
         //valorizzo il link del pdf in modo da distinguere risfa e sharepoint
         var urlToOpenPdf="";
 
         if(cellValue.docPdf_Key_Doc_RISFA){
-            urlToOpenPdf = URL_ENDPOINT+"/AFBNetWS/DocumentFileServlet?jSessionID="+window.sessionStorage.jsessionid+"&KeyDoc_RF="+cellValue.docPdf_Key_Doc_RISFA;
-            row$.append($$('<td data-collapsible-title="' + columns[3] + '"/>').html('<a href="#" class="doc-info_email" data-KeyDoc_RF="' +cellValue.docPdf_Key_Doc_RISFA+ '" data-doc_title="' + cellValue.docTitle + '" data-LinkUrlDocumento_SP=""><i class="f7-icons">email</i></a>'));
+            urlToOpenPdf = URL_ENDPOINT+"/AFBNetWS/DocumentFileServlet?jSessionID="+window.sessionStorage.jsessionid+"&KeyDoc_RF="+cellValue.docPdf_Key_Doc_RISFA;         
+            row$.append($$('<td data-collapsible-title="' + columns[4] + '"/>').html('<a href="#" class="doc-info_email" data-KeyDoc_RF="' +cellValue.docPdf_Key_Doc_RISFA+ '" data-doc_title="' + cellValue.docTitle + '" data-LinkUrlDocumento_SP=""><i class="f7-icons">email</i></a>'));
         }else if(cellValue.docPdf_LinkUrl_SHARE_POINT){
-            urlToOpenPdf = URL_ENDPOINT+"/AFBNetWS/DocumentFileServlet?jSessionID="+window.sessionStorage.jsessionid+"&LinkUrlDocumento_SP="+cellValue.docPdf_LinkUrl_SHARE_POINT;
-            row$.append($$('<td data-collapsible-title="' + columns[3] + '"/>').html('<a href="#" class="doc-info_email" data-KeyDoc_RF="" data-doc_title="' + cellValue.docTitle + '" data-LinkUrlDocumento_SP="'+cellValue.docPdf_LinkUrl_SHARE_POINT+'"><i class="f7-icons">email</i></a>'));
+            urlToOpenPdf = URL_ENDPOINT+"/AFBNetWS/DocumentFileServlet?jSessionID="+window.sessionStorage.jsessionid+"&LinkUrlDocumento_SP="+cellValue.docPdf_LinkUrl_SHARE_POINT;            
+            row$.append($$('<td data-collapsible-title="' + columns[4] + '"/>').html('<a href="#" class="doc-info_email" data-KeyDoc_RF="" data-doc_title="' + cellValue.docTitle + '" data-LinkUrlDocumento_SP="'+cellValue.docPdf_LinkUrl_SHARE_POINT+'"><i class="f7-icons">email</i></a>'));
         }
-
-
-        row$.append($$('<td data-collapsible-title="' + columns[4] + '"/>').html('<a href="#" class="doc-info_pdf" data-linkpdf="' + urlToOpenPdf + '"><i class="f7-icons">document_text_fill</i></a>'));
+        
+     
+        row$.append($$('<td data-collapsible-title="' + columns[5] + '"/>').html('<a href="#" class="doc-info_pdf" data-linkpdf="' + urlToOpenPdf + '"><i class="f7-icons">document_text_fill</i></a>'));
         $$(".data-table > table > tbody").append(row$);
     }
         $$('.doc-info_pdf').on('click', function (e) {
@@ -143,7 +144,7 @@ function buildDocumentTable(myList, columns, limit, lastIndexDoc) {
          //myApp.alert('url: '+linkPDF);
          if(linkPDF){
             //var ref = cordova.InAppBrowser.open(linkPDF, '_system', 'location=yes');
-          var ref = window.open(linkPDF, '_system', 'location=yes');
+          var ref = window.open(linkPDF, '_system', 'location=yes'); 
          }else{
              myApp.alert("Impossibile reperire il Pdf")
          }
@@ -155,7 +156,7 @@ function buildDocumentTable(myList, columns, limit, lastIndexDoc) {
             var keyDoc_RF = e.currentTarget.getAttribute("data-KeyDoc_RF");
             var linkUrlDocumento_SP = e.currentTarget.getAttribute("data-LinkUrlDocumento_SP");
             setTimeout(function () { sendDocument(keyDoc_RF, linkUrlDocumento_SP,title) }, 1000);
-
+            
         });
 
 }
@@ -200,15 +201,20 @@ function buildTicketTable(myList, columns, headers, limit, lastIndexDoc) {
     filteredList = undefined;
 }
 
-function searchDocWithFilters(docAmountFrom, docAmountTo, dateFrom, dateTo, docContains, limit, lastIndexDoc) {
+function searchDocWithFilters(docAmountFrom, docAmountTo, dateFrom, dateTo, docContains, docType, limit, lastIndexDoc) {
 
-    docTableData = getDocumentList(docAmountFrom,docAmountTo, dateFrom, dateTo, docContains);
+    docTableData = getDocumentList(docAmountFrom,docAmountTo, dateFrom, dateTo, docContains, docType);
+    // DOC in ordine desc
+    docTableData.sort(function(a,b) {
+        return new Date(b.DataDocumento).getTime() - new Date(a.DataDocumento).getTime() ; 
+    });
     if(docTableData){
         var documentCount = docTableData.length;
-        buildDocumentTable(docTableData, ['Numero', 'Nome', 'Data', 'E-mail', 'PDF'], limit, lastIndexDoc);
+        buildDocumentTable(docTableData, ['Numero', 'Tipo Documento', 'Data','Importo', 'E-mail', 'PDF'], limit, lastIndexDoc);
         if(documentCount || documentCount === 0){
             $$('.docCount').text(documentCount);
             $$('.documentSearchCount').show();
+            $('.page-content').animate({scrollTop: 330}, 500);
         }
     }else{
        return;
@@ -226,7 +232,7 @@ function toFilterTickets(dateFrom, dateTo, status, desc){
     }
 
     var stringFilters = 'oslc.pageSize='+pageSizeFilterTickets+'&oslc.orderBy='+orderByFilterTickets+'&oslc.select=*&oslc.where=reportedby="'+window.sessionStorage.username+'" and creationdate>="'+dateFrom+'" and creationdate<="'+dateTo+'" and status="'+status+'"'+descIfExist;
-
+    
     return stringFilters;
 }
 
@@ -269,7 +275,7 @@ function dataURItoBlob(dataURI) {
     return blob;
 }
 function populateTicketPageDetails(ticket){
-
+   
     var assignment = ticket.assignment2 ? ticket.assignment2 : ticket.assignment1;
       if(!assignment){
           assignment = 'Operatore non disponibile';
@@ -279,14 +285,14 @@ function populateTicketPageDetails(ticket){
             var tmp = desc.split("__");
             desc = tmp[0];
         }
-
+    
     $$(".hrefTicketId").val(ticket.href);
     $$(".textAreaRichiestaTkt").val(desc);
     $$(".textAreaDettagliTkt").val(ticket.description_longdescription ? ticket.description_longdescription.replace(/<(?:.|\n)*?>/gm, '') : "Dettaglio ticket non disponibile");
     $$(".statusTkt input").val(ticket.status ? ticket.status : "Status non disponibile");
     $$(".operatoreTkt input").val(assignment);
     $$(".textAreaSoluzioneTkt").val(ticket.fr2code_longdescription  ? ticket.fr2code_longdescription.replace(/<(?:.|\n)*?>/gm, '')  : "Dettaglio risoluzione non disponibile");
-
+    
     if((ticket.val1 || ticket.val2 || ticket.cordialita) && ticket.status == 'RESOLVED'){
         $$("#btn-valuta-ticket").hide();
         $$(".valutazioneTkt").hide();
@@ -297,7 +303,7 @@ function populateTicketPageDetails(ticket){
     if(ticket.status !== 'RESOLVED' && ticket.status !== 'CLOSED'){
         $$(".soluzioneTicket").hide();
     }
-
+    
     if(ticket.status !== 'RESOLVED'){
         $$(".valutazioneTkt").hide();
         $$("#btn-valuta-ticket").hide();
@@ -309,7 +315,7 @@ function prepareEval(){
     var valutazioneTempistica = parseInt($$('input[name=tempistica]:checked').val());
     var valutazioneSoluzione = parseInt($$('input[name=soluzione]:checked').val());
     var valutazioneCortesia = parseInt($$('input[name=cortesia]:checked').val());
-    if((valutazioneCortesia < 3 || valutazioneSoluzione < 3 || valutazioneTempistica < 3) && $$(".notaValutazione textarea").val() === ''){
+    if((valutazioneCortesia < 3 || valutazioneSoluzione < 3 || valutazioneTempistica < 3) && $$(".notaValutazione textarea").val() === ''){   
         myApp.alert("Inserisci una nota di valutazione","Nota Valutazione");
         $$(".notaValutazione").css("display","block");
         myApp.hidePreloader();
@@ -319,7 +325,7 @@ function prepareEval(){
         sendEval(valutazioneTempistica, valutazioneSoluzione, valutazioneCortesia, notaValutazione, hrefTicket);
     }
 }
-
+ 
 function blockAfterEval(){
 
         $$(".valutazioneTkt input").attr({
@@ -341,7 +347,7 @@ function setUserProfile(data){
             groupArray.push(value.toLowerCase());
         }
     });
-
+    
    window.sessionStorage.setItem("userProfile", groupArray);
 }
 function verifyUserProfile(){
@@ -356,4 +362,13 @@ function verifyUserProfile(){
         }else{
             $$(".gestioneTicket").show();
         }
+}
+
+function formatAmountToFloat(amount){
+    var amountFixed2 = "Non Disponibile";
+    var tmp = parseFloat(amount);
+    if(tmp){
+        amountFixed2 = tmp.toFixed(2);
+    }
+    return amountFixed2;
 }
